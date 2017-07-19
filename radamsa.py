f30786586ddaa4eef2659435793134e0badeafc5
root <root@localhost.localdomain>
2017-07-19 05:08:46 -0400

    params for the script

--- radamsa.py
+++ radamsa.py
@@ -3,8 +3,12 @@ import sys
 import ctypes
 from ctypes import *
 
+if len(sys.argv) < 2:
+    print "Usage: radamsa.py <string_to_mutate>"
+    sys.exit(1)
+
 test = ctypes.cdll.LoadLibrary("./radamsa.so")
 print test
 test.restype = c_char_p
-result = test.Mutate(b'tr')
+result = test.Mutate(sys.argv[1])
 print hex(result)
